9def26b644f549d0182ed16db66833de38c7e5e0
gpyo0111 <gpyo0111@naver.com>
2026-05-26 16:08:40 +0900

    modified 20-runtime variables

--- 20-runtime/variables.tf
+++ 20-runtime/variables.tf
@@ -23,40 +23,6 @@ variable "env" {
   default = "dev"
 }
 
-variable "vpc_id" {
-  type    = string
-  default = "vpc-040923e167f09c210"
-}
-
-variable "public_subnet_ids" {
-  type = list(string)
-  default = [
-    "subnet-06cef727fab3f2704",
-    "subnet-0457fb1c76573dcd1"
-  ]
-}
-
-variable "private_app_subnet_ids" {
-  type = list(string)
-  default = [
-    "subnet-0bc568164c640b910",
-    "subnet-00b3943c9a029e5c2"
-  ]
-}
-
-variable "private_app_route_table_id" {
-  type    = string
-  default = "rtb-032678c4d7692558c"
-}
-
-variable "private_app_subnet_cidrs" {
-  type = list(string)
-  default = [
-    "10.0.11.0/24",
-    "10.0.12.0/24"
-  ]
-}
-
 variable "audio_bucket_name" {
   type    = string
   default = "mzc-securevoiceguard-audio-dev-455535733131-ap-northeast-2-an"
